9c4d94c9bdc39a614d839b2beb9f70e11d29d260
Eugene Ignashov <eugene.ign@yandex.ru>
2022-03-25 16:35:38 +0500

    добавил файнал пÐк полямÐ

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,19 +6,14 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
-
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 
 public class Util {
-    private static String URL = "jdbc:mysql://localhost:3306/database?useSSL=false&serverTimezone=UTC";
-    private static String USERNAME = "root";
-    private static String PASSWORD = "root";
-    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/database?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     private static SessionFactory sessionFactory;
 
